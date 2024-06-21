61b0a397b00d042bd7682e49b60256eb3cb1baf4
Vincent Rémond <vincent.remond@gmail.com>
2024-06-21 18:51:57 +0200

    Refactor directory and file rule structure for improved readability

--- CleanupUserProfile/Program.fs
+++ CleanupUserProfile/Program.fs
@@ -22,6 +22,7 @@ and Action =
     | Noop
     | Unlink
     | Delete
+    | DeleteRecursive
 
 and Condition =
     | Any
@@ -100,6 +101,12 @@ let applyAction (action: Action) (item: FileSystemInfo) =
     | Unlink ->
         printfn $"Unlinking \"{item.FullName}\""
         item.Delete()
+    | DeleteRecursive ->
+        match item with
+        | :? DirectoryInfo as dir ->
+            printfn $"Deleting dir recursively \"{item.FullName}\""
+            dir.Delete(true)
+        | _ -> failwith "Cannot delete recursively something that is not a DirectoryInfo"
     | Delete ->
         match item with
         | :? DirectoryInfo as dir ->
@@ -111,9 +118,11 @@ let applyAction (action: Action) (item: FileSystemInfo) =
         | _ -> failwith "Cannot delete something that is not a DirectoryInfo or a FileInfo"
 
 let rec run (folder: DirectoryInfo) (foldersRules: FolderRule list) (filesRules: FileRule list) =
+    (runFolders folder foldersRules) @ (runFiles folder filesRules)
 
+and runFolders folder foldersRules : FileSystemInfo list =
     folder.GetDirectories()
-    |> Seq.iter (fun dirInfo ->
+    |> Seq.collect (fun dirInfo ->
         let rule =
             foldersRules
             |> List.tryPick (fun folderRule ->
@@ -126,20 +135,28 @@ let rec run (folder: DirectoryInfo) (foldersRules: FolderRule list) (filesRules:
         match rule with
         | Some folderRule ->
 
-            match folderRule.FolderChildRules with
-            | FolderChildRules.Process(foldersRules, filesRules) -> run dirInfo foldersRules filesRules
-            | FolderChildRules.Noop -> ()
+            let childResults =
+                match folderRule.FolderChildRules with
+                | FolderChildRules.Process(foldersRules, filesRules) -> run dirInfo foldersRules filesRules
+                | FolderChildRules.Noop -> []
 
-            applyAction folderRule.SelfAction dirInfo
+            // If no issue with the child, apply the action on the folder
+            if List.isEmpty childResults then
+                applyAction folderRule.SelfAction dirInfo
 
-        | None -> printfn $"What to do with DIR  {dirInfo}"
+            childResults
+
+        | None ->
+            [ (dirInfo :> FileSystemInfo) ]
     )
+    |> Seq.toList
 
+and runFiles folder filesRules : FileSystemInfo list =
     folder.GetFiles()
-    |> Seq.iter (fun fileInfo ->
+    |> Seq.choose (fun fileInfo ->
 
         if testCondition (Name(Eq "desktop.ini")) fileInfo then
-            ()
+            None
         else
 
             let rule =
@@ -152,9 +169,13 @@ let rec run (folder: DirectoryInfo) (foldersRules: FolderRule list) (filesRules:
                 )
 
             match rule with
-            | Some fileRule -> applyAction fileRule.Action fileInfo
-            | None -> printfn $"What to do with FILE {fileInfo}"
+            | Some fileRule ->
+                applyAction fileRule.Action fileInfo
+                None
+            | None ->
+                Some(fileInfo :> FileSystemInfo)
     )
+    |> Seq.toList
 
 let file condition action = {
     Condition = condition
@@ -175,120 +196,123 @@ let dir' condition action = {
 
 let ignoreFolders = [ dir Any ]
 
-run userProfile [
-    dir'
-        (Or [
-            Name(StartsWith ".")
-            Name(StartsWith "_")
-        ])
-        Hide
-    dir'
-        (Or [
-            Name(Eq "AppData")
-            Name(Eq "Apps")
-            Name(Eq "Data")
-            Name(Eq "repos")
-            Name(Eq "tmp")
-            Name(Eq "OneDrive")
-            Name(StartsWith "OneDrive -")
-        ])
-        Noop
-    dir (Name(Eq "Downloads")) Noop [] []
-    dir
-        (And [
-            IsSymLink
-            Or [
-                Name(Eq @"Application Data")
-                Name(Eq @"Cookies")
-                Name(Eq @"Local Settings")
-                Name(Eq @"Menu Démarrer")
-                Name(Eq @"Mes documents")
-                Name(Eq @"Modèles")
-                Name(Eq @"My Documents")
-                Name(Eq @"NetHood")
-                Name(Eq @"PrintHood")
-                Name(Eq @"Recent")
-                Name(Eq @"SendTo")
-                Name(Eq @"Start Menu")
-                Name(Eq @"Templates")
-                Name(Eq @"Voisinage d'impression")
-                Name(Eq @"Voisinage réseau")
-            ]
-        ])
-        Unlink [] []
-    dir (Name(Eq "dotTraceSnapshots")) Delete [
-        dir (Name(Eq "Temp")) Delete [] []
-        dir (Name(StartsWith "Unit Tests ")) Delete [] [ file (Name(Match @"\.tmp(\.\d+)?$")) Delete ]
-    ] []
-    dir (Name(Eq "Contacts")) Hide [] []
-    dir (Name(Eq "Links")) Hide [] [ file (Extension(Eq ".lnk")) Noop ]
-    dir (Name(Eq "Pictures")) Hide [
-        dir' (Name(Eq "Screenpresso")) Noop
-        dir (Name(Eq "Wallpapers")) Noop [] []
-        dir (Name(Eq "Camera Roll")) Noop [] []
-        dir (Name(Eq "Saved Pictures")) Noop [] []
-        dir (Name(Eq "Feedback")) Delete [
-            dir (Name(Match @"^\{[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}\}$")) Delete [] [ file (Name(Match @"\.png$")) Delete ]
-        ] []
-    ] []
-    dir (Name(Eq "Music")) Hide [] []
-    dir (Name(Eq "Videos")) Hide [
-        dir (Name(Eq "Captures")) Noop [] [ file (Extension(Eq ".mp4")) Delete ]
-        dir (Name(Eq "AnyDesk")) Delete [] []
-    ] []
-    dir (Name(Eq "Searches")) Hide [] [
-        file (Extension(Eq ".search-ms")) Noop
-        file (Extension(Eq ".searchconnector-ms")) Noop
-    ]
-    dir (Name(Eq "Saved Games")) Hide [] []
-    dir (Name(Eq "ai_overlay_tmp")) Hide [] []
-    dir (Name(Eq "Desktop")) Noop [] [
-        file (Extension(Eq ".lnk")) Delete
-        file (Extension(Eq ".url")) Delete
-    ]
-    dir' (Name(Eq "Favorites")) Noop
-    dir' (Name(Eq "IntelGraphicsProfiles")) Hide
-    dir' (Name(Eq "Perso")) Noop
-    dir (Name(Eq "Postman")) Delete [ dir (Name(Eq "files")) Delete [] [] ] []
-    dir (Name(Eq "nuget")) Noop [] [ file (Extension(Eq ".nupkg")) Noop ]
-    dir (Name(Eq "source")) Delete [ dir (Name(Eq "repos")) Delete [] [] ] []
-    dir (Name(Eq "Documents")) Noop [
-        dir (Name(Eq "Custom Office Templates")) Delete [] []
-        dir' (Name(Eq "Fiddler2")) Noop
-        dir' (Name(Eq "Dell")) Noop
-        dir (Name(Eq "PowerToys")) Delete [ dir (Name(Eq "Backup")) Delete [] [] ] []
-        dir
-            (Or [
-                Name(Eq "Ma musique")
-                Name(Eq "Mes images")
-                Name(Eq "Mes vidéos")
-                Name(Eq "My Music")
-                Name(Eq "My Pictures")
-                Name(Eq "My Videos")
-            ])
-            Unlink [] []
+let notProcessedItems =
+    run userProfile [
         dir'
             (Or [
-                Name(Eq "IISExpress")
-                Name(Eq "PowerShell")
-                Name(Eq "WindowsPowerShell")
-                Name(Eq "My Web Sites")
-                Name(Eq "Visual Studio 2017")
-                Name(Eq "Visual Studio 2022")
+                Name(StartsWith ".")
+                Name(StartsWith "_")
+            ])
+            Hide
+        dir'
+            (Or [
+                Name(Eq "AppData")
+                Name(Eq "Apps")
+                Name(Eq "Data")
+                Name(Eq "repos")
+                Name(Eq "tmp")
+                Name(Eq "OneDrive")
+                Name(StartsWith "OneDrive -")
             ])
             Noop
-        dir (Name(Eq "Fichiers Outlook")) Noop [] []
-        dir (Name(Eq "Zoom")) Noop [] []
-    ] [ file (Name(Eq "Default.rdp")) Hide ]
-] [
-    file (Name(Eq ".editorconfig")) Noop
-    file
-        (Or [
-            Name(StartsWith ".")
-            Name(StartsWith "_")
-        ])
-        Hide
+        dir (Name(Eq "Downloads")) Noop [] []
+        dir
+            (And [
+                IsSymLink
+                Or [
+                    Name(Eq @"Application Data")
+                    Name(Eq @"Cookies")
+                    Name(Eq @"Local Settings")
+                    Name(Eq @"Menu Démarrer")
+                    Name(Eq @"Mes documents")
+                    Name(Eq @"Modèles")
+                    Name(Eq @"My Documents")
+                    Name(Eq @"NetHood")
+                    Name(Eq @"PrintHood")
+                    Name(Eq @"Recent")
+                    Name(Eq @"SendTo")
+                    Name(Eq @"Start Menu")
+                    Name(Eq @"Templates")
+                    Name(Eq @"Voisinage d'impression")
+                    Name(Eq @"Voisinage réseau")
+                ]
+            ])
+            Unlink [] []
+        dir' (Name(Eq "dotTraceSnapshots")) DeleteRecursive
+        dir' (Name(Eq "RiderSnapshots")) DeleteRecursive
+        dir (Name(Eq "Contacts")) Hide [] []
+        dir (Name(Eq "Links")) Hide [] [ file (Extension(Eq ".lnk")) Noop ]
+        dir (Name(Eq "Pictures")) Hide [
+            dir' (Name(Eq "Screenpresso")) Noop
+            dir (Name(Eq "Wallpapers")) Noop [] []
+            dir (Name(Eq "Camera Roll")) Noop [] []
+            dir (Name(Eq "Saved Pictures")) Noop [] []
+            dir (Name(Eq "Feedback")) Delete [
+                dir (Name(Match @"^\{[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}\}$")) Delete [] [ file (Name(Match @"\.png$")) Delete ]
+            ] []
+        ] []
+        dir (Name(Eq "Music")) Hide [] []
+        dir (Name(Eq "Videos")) Hide [
+            dir (Name(Eq "Captures")) Noop [] [ file (Extension(Eq ".mp4")) Delete ]
+            dir (Name(Eq "AnyDesk")) Delete [] []
+        ] []
+        dir (Name(Eq "Searches")) Hide [] [
+            file (Extension(Eq ".search-ms")) Noop
+            file (Extension(Eq ".searchconnector-ms")) Noop
+        ]
+        dir (Name(Eq "Saved Games")) Hide [] []
+        dir (Name(Eq "ai_overlay_tmp")) Hide [] []
+        dir (Name(Eq "Desktop")) Noop [] [
+            file (Extension(Eq ".lnk")) Delete
+            file (Extension(Eq ".url")) Delete
+        ]
+        dir' (Name(Eq "Favorites")) Noop
+        dir' (Name(Eq "IntelGraphicsProfiles")) Hide
+        dir' (Name(Eq "Perso")) Noop
+        dir (Name(Eq "Postman")) Delete [ dir (Name(Eq "files")) Delete [] [] ] []
+        dir (Name(Eq "nuget")) Noop [] [ file (Extension(Eq ".nupkg")) Noop ]
+        dir (Name(Eq "source")) Delete [ dir (Name(Eq "repos")) Delete [] [] ] []
+        dir (Name(Eq "Documents")) Noop [
+            dir (Name(Eq "FeedbackHub")) Delete [] []
+            dir (Name(Eq "Custom Office Templates")) Delete [] []
+            dir' (Name(Eq "Fiddler2")) Noop
+            dir' (Name(Eq "Dell")) Noop
+            dir (Name(Eq "PowerToys")) Delete [ dir (Name(Eq "Backup")) Delete [] [] ] []
+            dir
+                (Or [
+                    Name(Eq "Ma musique")
+                    Name(Eq "Mes images")
+                    Name(Eq "Mes vidéos")
+                    Name(Eq "My Music")
+                    Name(Eq "My Pictures")
+                    Name(Eq "My Videos")
+                ])
+                Unlink [] []
+            dir'
+                (Or [
+                    Name(Eq "IISExpress")
+                    Name(Eq "PowerShell")
+                    Name(Eq "WindowsPowerShell")
+                    Name(Eq "My Web Sites")
+                    Name(Eq "Visual Studio 2017")
+                    Name(Eq "Visual Studio 2022")
+                ])
+                Noop
+            dir (Name(Eq "Fichiers Outlook")) Noop [] []
+            dir (Name(Eq "Zoom")) Noop [] []
+        ] [ file (Name(Eq "Default.rdp")) Hide ]
+    ] [
+        file (Name(Eq ".editorconfig")) Noop
+        file
+            (Or [
+                Name(StartsWith ".")
+                Name(StartsWith "_")
+            ])
+            Hide
 
-    file (Name(StartsWith @"NTUSER.")) Noop
-    file (Name(Match @"^AzureStorageEmulatorDb\d+(_log)?.(ldf|mdf)$")) Hide
-]
+        file (Name(StartsWith @"NTUSER.")) Noop
+        file (Name(Match @"^AzureStorageEmulatorDb\d+(_log)?.(ldf|mdf)$")) Hide
+    ]
+
+for item in notProcessedItems do
+    printfn $"What to do with : {item.FullName}"
