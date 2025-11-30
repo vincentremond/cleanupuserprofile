3b3f4620ba46eda62781043e36db5e6774a78e7b
Vincent Rémond <vincent.remond@gmail.com>
2025-11-30 11:32:55 +0100

    chore: quickstart

--- CleanupUserProfile/Program.fs
+++ CleanupUserProfile/Program.fs
@@ -47,7 +47,8 @@ let applyDirectoryAction (action: DirectoryAction) (directoryInfo: DirectoryInfo
         AnsiConsole.markupLineInterpolated $"[blue]Unlinking[/] \"[bold white]{directoryInfo.FullName}[/]\""
         directoryInfo.Delete()
     | D.DeleteRecursive ->
-        AnsiConsole.markupLineInterpolated $"[blue]Deleting dir recursively[/] \"[bold white]{directoryInfo.FullName}[/]\""
+        AnsiConsole.markupLineInterpolated
+            $"[blue]Deleting dir recursively[/] \"[bold white]{directoryInfo.FullName}[/]\""
 
         directoryInfo.Delete(true)
     | D.ContainsNoFiles ->
@@ -84,7 +85,8 @@ let rec applyFileAction (fileInfo: FileInfo) (action: FileAction) =
             fileInfo.Delete()
         with
         | :? IOException as ex ->
-            AnsiConsole.markupLineInterpolated $"[yellow]Cannot delete file[/] \"[bold white]{fileInfo.FullName}[/]\" because {ex.Message}"
+            AnsiConsole.markupLineInterpolated
+                $"[yellow]Cannot delete file[/] \"[bold white]{fileInfo.FullName}[/]\" because {ex.Message}"
         | ex -> raise ex
 
         fileInfo
@@ -98,7 +100,8 @@ let rec applyFileAction (fileInfo: FileInfo) (action: FileAction) =
 
         let targetFileFullPath = computedTargetDirectory <?/> fileInfo.Name
 
-        AnsiConsole.markupLineInterpolated $"[blue]Moving file[/] \"[bold white]{fileInfo.FullName}[/]\" to \"[bold white]{target}[/]\""
+        AnsiConsole.markupLineInterpolated
+            $"[blue]Moving file[/] \"[bold white]{fileInfo.FullName}[/]\" to \"[bold white]{target}[/]\""
 
         fileInfo.MoveTo(targetFileFullPath.FullName, overwrite = false)
         targetFileFullPath
@@ -136,7 +139,8 @@ and runDirectorys directory directorysRules : FileSystemInfo list =
 
             childResults
         | rules ->
-            AnsiConsole.markupLineInterpolated $"[red]Multiple rules match for[/] \"[bold white]{dirInfo.FullName}[/]\""
+            AnsiConsole.markupLineInterpolated
+                $"[red]Multiple rules match for[/] \"[bold white]{dirInfo.FullName}[/]\""
 
             AnsiConsole.markupLineInterpolated $"[red]Rules:[/]\n{rules |> List.map string |> String.concatC '\n'}"
             [ (dirInfo :> FileSystemInfo) ]
@@ -162,7 +166,8 @@ and runFiles directory filesRules : FileSystemInfo list =
                 applyFileAction fileInfo fileRule.Action |> ignore
                 None
             | rules ->
-                AnsiConsole.markupLineInterpolated $"[red]Multiple rules match for[/] \"[bold white]{fileInfo.FullName}[/]\""
+                AnsiConsole.markupLineInterpolated
+                    $"[red]Multiple rules match for[/] \"[bold white]{fileInfo.FullName}[/]\""
 
                 AnsiConsole.markupLineInterpolated $"[red]Rules:[/]\n{rules |> List.map string |> String.concatC '\n'}"
                 Some(fileInfo :> FileSystemInfo)
